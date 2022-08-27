78385506e09cb9d927a45bcd0f9da4efa23f9083
dustin4242 <thegameexposer@gmail.com>
2022-08-27 03:03:44 -0500

    Added if statements in compile shell script so it tells you when you dont pass parameters

--- compilerun.sh
+++ compilerun.sh
@@ -1,5 +1,10 @@
-bun src/main.ts $1
-rustc -C opt-level=3 main.rs
-rm -rf main.rs
-chmod +x ./main
-./main
+if [ $1 ] && [ -r $1 ]
+then
+	bun src/main.ts $1
+	rustc -C opt-level=3 main.rs
+	rm -rf main.rs
+	chmod +x ./main
+	./main
+else
+	echo 'Pass parameter into compilerun script ex: "./compilerun.sh ex.sus"'
+fi
